b3e6cb26367e88bfb2e5e0d886c59368b942752a
VictorSauceda <victorasauceda@gmail.com>
2019-09-17 16:20:42 -0700

    testerredis

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true }));
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
